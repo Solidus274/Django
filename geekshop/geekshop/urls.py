5c4fe76afc2e9d278208988bc4968ac96a9e41b1
Solidus274 <solidus274@gmail.com>
2020-04-22 00:30:23 +0300

    Выполнено

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -25,9 +25,9 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', views.contact, name='contact'),
-    path('admin/', admin.site.urls),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
+    path('admin/', include('adminapp.urls', namespace='admin'))
 ]
 
 if settings.DEBUG:
