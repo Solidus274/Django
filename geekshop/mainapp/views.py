233748d412bb4068d9821d0a54f1244376a0929a
Solidus274 <solidus274@gmail.com>
2020-04-25 21:00:59 +0300

    Задание выполнено
    Прошу помочь с контролерами по продуктам. Неверно отрабатывают кнопки Сохранить и удалить, так как не могу корректно передать в продуктах номер категории этого продукта. Также не понятно как создать новый продукт в рамках обзора продуктов катеогрии, так как номера у продукта ее нет

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 import random
 from basketapp.models import Basket
 from .models import ProductCategory, Product
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def main(request):
@@ -37,12 +37,39 @@ def get_same_products(hot_product):
     return same_products
 
 
-def products(request, pk=None):
+def products(request, pk=None, page=1):
     title = 'продукты'
-    links_menu = ProductCategory.objects.all()
+    links_menu = ProductCategory.objects.filter(is_active=True)
     basket = get_basket(request.user)
 
-    ...
+    if pk is not None:
+        if pk == 0:
+            category = {
+                'pk': 0,
+                'name': 'все'
+            }
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
+
+        paginator = Paginator(products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products_paginator,
+            'basket': basket,
+        }
+
+        return render(request, 'products_list.html', content)
 
     hot_product = get_hot_product()
     same_products = get_same_products(hot_product)
