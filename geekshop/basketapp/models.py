3c89f3de38d30a9a38efb7f7f7d37bdf74592138
Solidus274 <solidus274@gmail.com>
2020-05-15 18:59:19 +0300

    Выполнено

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -4,12 +4,23 @@ from django.db import models
 from django.conf import settings
 from mainapp.models import Product
 
+# class BasketQuerySet(models.QuerySet):
+#
+#    def delete(self, *args, **kwargs):
+#        for object in self:
+#            object.product.quantity += object.quantity
+#            object.product.save()
+#        super(BasketQuerySet, self).delete(*args, **kwargs)
+
 
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+    # objects = BasketQuerySet.as_manager()
+
+
 
     def product_cost(self):
         "return cost of all products this type"
@@ -21,8 +32,7 @@ class Basket(models.Model):
         _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
         return _totalquantity
 
-    @staticmethod
-    def total_cost(self):
+    def get_total_cost(self):
         "return total cost for user"
         _items = Basket.objects.filter(user=self.user)
         _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
@@ -41,4 +51,29 @@ class Basket(models.Model):
     #     basket_items = cls.get_items(user)
     #     basket_items_dic = {}
     #     [basket_items_dic.update({item.product: item.quantity}) for item in basket
+
+    # return basket_items_dic
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.filter(pk=pk).order_by('product__category')
+
+    # def save(self, *args, **kwargs):
+    #     if self.pk:
+    #         self.product.quantity -= self.quantity - \
+    #                                  self.__class__.get_item(self.pk).quantity
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     self.product.save()
+    #     super(self.__class__, self).save(*args, **kwargs)
+
+    # def delete(self):
+    #     self.product.quantity += self.quantity
+    #     self.product.save()
+    #     super(self.__class__, self).delete()
+
+
+
+
+
 
--- geekshop/ordersapp/forms.py
+++ geekshop/ordersapp/forms.py
@@ -3,22 +3,24 @@ from ordersapp.models import Order, OrderItem
 
 
 class OrderForm(forms.ModelForm):
-   class Meta:
-       model = Order
-       exclude = ('user',)
+    class Meta:
+        model = Order
+        exclude = ('user',)
 
-   def __init__(self, *args, **kwargs):
-       super(OrderForm, self).__init__(*args, **kwargs)
-       for field_name, field in self.fields.items():
-           field.widget.attrs['class'] = 'form-control'
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 
 class OrderItemForm(forms.ModelForm):
-   class Meta:
-       model = OrderItem
-       exclude = ()
+    price = forms.CharField(label='цена', required=False)
 
-   def __init__(self, *args, **kwargs):
-       super(OrderItemForm, self).__init__(*args, **kwargs)
-       for field_name, field in self.fields.items():
-           field.widget.attrs['class'] = 'form-control'
+    class Meta:
+        model = OrderItem
+        exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super(OrderItemForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
