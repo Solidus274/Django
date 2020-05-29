499779b014e92e25bb54a216c481a85691da1833
Solidus274 <solidus274@gmail.com>
2020-05-29 19:18:33 +0300

    Задание 7 выполнено

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,12 +1,21 @@
-from django.urls import path
-from django.urls import re_path
-import mainapp.views as mainapp
+# from django.conf.urls import url
 
-app_name = 'mainapp'
+import mainapp.views as mainapp
+from django.urls import re_path
+
+from django.views.decorators.cache import cache_page
+
+app_name="mainapp"
 
 urlpatterns = [
     re_path(r'^$', mainapp.products, name='index'),
     re_path(r'^category/(?P<pk>\d+)/$', mainapp.products, name='category'),
+    re_path(r'^category/(?P<pk>\d+)/ajax/$', cache_page(3600)(mainapp.products_ajax)),
     re_path(r'^product/(?P<pk>\d+)/$', mainapp.product, name='product'),
+
     re_path(r'^category/(?P<pk>\d+)/page/(?P<page>\d+)/$', mainapp.products, name='page'),
+    re_path(r'^category/(?P<pk>\d+)/page/(?P<page>\d+)/ajax/$', cache_page(3600)(mainapp.products_ajax)),
+
 ]
+
+    
